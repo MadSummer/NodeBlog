eaf21ceb9d541e925679c05233b8166130a2f478
MadSummer <xhttkllj@163.com>
2016-07-19 23:33:58 +0800

    样式修改，后台view的搭建

--- source/app.js
+++ source/app.js
@@ -20,6 +20,7 @@ let userpage = require('./routes/userpage');
 let post = require('./routes/post');
 let upload = require('./routes/upload');
 let search = require('./routes/search');
+let admin = require('./routes/admin');
 /*实例化app为一个express应用*/
 let app = express();
 
@@ -29,10 +30,10 @@ app.set('view engine', 'ejs');
 app.use(session({
   secret: settings.cookieSecret,
   key: settings.db,//cookie name
-  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
-    store:new MongoStore({
-       url: 'mongodb://localhost/blog'
-    })
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 },//30 days
+  store: new MongoStore({
+    url: 'mongodb://localhost/blog'
+  })
 }));
 app.use(flash());
 // uncomment after placing your favicon in /public
@@ -49,10 +50,11 @@ app.use('/logout', index);
 app.use('/u', userpage);
 app.use('/p', article);
 app.use('/post', post);
-app.use('/upload',upload);
-app.use('/search',search);
+app.use('/upload', upload);
+app.use('/search', search);
+app.use('/admin',admin);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   let err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -63,7 +65,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -74,35 +76,35 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
     error: {}
   });
 });
-app.locals['formatDate'] = (_id,format)=>{
+app.locals['formatDate'] = (_id, format) => {
   'use strict';
-  let stamp = new Date(parseInt(_id.toString().substring(0,8),16)*1000);
+  let stamp = new Date(parseInt(_id.toString().substring(0, 8), 16) * 1000);
   let year = stamp.getFullYear();
   let month = stamp.getMonth() + 1;
   let day = stamp.getDate();
   let hour = stamp.getHours();
   let minute = stamp.getMinutes();
   let second = stamp.getSeconds();
-  if(second < 10){
-    second = '0' + second ;
+  if (second < 10) {
+    second = '0' + second;
   }
-  if(minute < 10){
-    minute = '0' + minute ; 
+  if (minute < 10) {
+    minute = '0' + minute;
   }
-  if(hour < 10){
-    hour = '0' + hour ; 
+  if (hour < 10) {
+    hour = '0' + hour;
   }
-  switch (format){
-    case 1 : return year + '年' + month + '月' + day + '日' + hour + '时' + minute + '分' + second + '秒';
-    case 2 : return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
-    case 3 : return year + '/' + month + '/' + day + '/' + hour + '/' + minute + '/' + second;
+  switch (format) {
+    case 1: return year + '年' + month + '月' + day + '日' + hour + '时' + minute + '分' + second + '秒';
+    case 2: return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+    case 3: return year + '/' + month + '/' + day + '/' + hour + '/' + minute + '/' + second;
     default: return year + '年' + month + '月' + day + '日' + hour + '时' + minute + '分' + second + '秒';
   }
 };
